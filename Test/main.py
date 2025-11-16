0f7226c0eb5644c75f96c3fa14d2b61dd27adf71
Kirill Zhuchenko <kirillzhuch@gmail.com>
2025-11-16 11:15:59 -0800

    Update main.py

--- Test/main.py
+++ Test/main.py
@@ -3,8 +3,8 @@ import requests
 
 api = P2P(
     testnet=True,
-    api_key="IjQCejlmaMcdhbEhy8",
-    api_secret="zpjDLwnHzGkoU7Lo0axxIXYGd5t3UPhsUbEi"
+    api_key="IjQCecdhbEhy8",
+    api_secret="zEi"
 )
 
 # 1. Get current balance
