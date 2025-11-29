371e76b83610c452711cac2e8313167708ecca17
Kirill Zhuchenko <kirillzhuch@gmail.com>
2025-11-29 09:30:25 -0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Bybit_P2P_Test/main.py

--- Bybit_P2P_Test/main.py
+++ Bybit_P2P_Test/main.py
@@ -1,3 +1,10 @@
+'''cannot use mark_as_paid() with USD on Wise, not sure about Revolut. Should be good with EUR'''
+
+"""Removed func():
+fetch_test_ad_details(), fetch_ads_list(), fetch_wise_buy_ad(), fetch_wise_sell_ad(), fetch_list_of_sell_orders(),
+fetch_bybit_counterparty_info()"""
+
+
 from async_bybit_p2p import P2P
 import asyncio
 import os
@@ -12,21 +19,6 @@ async def fetch_balance(client: P2P):
     present_balance = current_balance["result"]["balance"][2]["transferBalance"]
     return present_balance
 
-async def fetch_ads_list(client: P2P): #### idk if I need this one.
-    # I probably have to update the same ad continuously to meet present Wise balance.
-    ads_list = await client.get_ads_list()
-    wise_ad = ads_list["result"]["items"][0]
-    return ads_list["result"]["items"]
-
-async def fetch_wise_buy_ad(client: P2P): ## Not sure if I need this one
-    ads_list = await fetch_ads_list(client=client)
-    wise_buy_ad = ads_list[0]
-    return wise_buy_ad
-
-async def fetch_wise_sell_ad(client: P2P): ##Not sure if I need this one
-    ads_list = await fetch_ads_list(client=client)
-    wise_sell_ad = ads_list[1]
-    return wise_sell_ad
 
 async def fetch_wise_buy_ad_details(client: P2P):
     wise_buy_ad = await client.get_ad_details(itemId="1799865939992154112")
@@ -36,10 +28,7 @@ async def fetch_wise_sell_ad_details(client: P2P):
     wise_sell_ad = await client.get_ad_details(itemId="1989351720308887552")
     return wise_sell_ad
 
-"""DELETE TEST AD IN THE FUTURE"""
-async def fetch_test_ad_details(client: P2P):
-    test_ad = await client.get_ad_details(itemId="1977382182365315072")
-    return test_ad
+
 
 """USES TEST AD AT THIS MOMENT, HAVE TO CHANGE TO REAL ONE BEFORE GO LIVE"""
 async def modify_wise_buy_ad(client: P2P):  #Uses test ad at this stage
@@ -121,7 +110,7 @@ async def activate_wise_buy_ad(client: P2P):
         price=0.97,  # Adjust price before going live
         minAmount=150,
         maxAmount=200,
-        remark="Contact @kolya5544 on Telegram once you've paid.",  # Remark shall be changed in the future
+        remark="Contact @kol4 on Telegram once you've paid.",  # Remark shall be changed in the future
         tradingPreferenceSet={
             "hasUnPostAd": 0,
             "isKyc": 1,
@@ -155,7 +144,7 @@ async def activate_wise_sell_ad(client: P2P):
         price=1.05, # Adjust price before going live
         minAmount=150,
         maxAmount=200,
-        remark="Contact @kolya5544 on Telegram once you've paid.",  # Remark shall be changed in the future
+        remark="Contact @ko44 on Telegram once you've paid.",  # Remark shall be changed in the future
         tradingPreferenceSet={
             "hasUnPostAd": 0,
             "isKyc": 1,
@@ -191,30 +180,6 @@ async def remove_wise_sell_ad(client: P2P):
     )
     return sell_ad_rem
 
-"""NEXT 2 func to be removed in the future, both are handled in fetch_pending_buy/sell_orders"""
-async def fetch_list_of_sell_orders(client: P2P):
-    list_of_orders = await client.get_orders(
-        page=1,
-        size=10,
-        side=1
-    )
-    list = list_of_orders["result"]["items"]
-    return list
-
-
-async def fetch_bybit_counterparty_info(client: P2P):
-    orders = await fetch_list_of_sell_orders(client=client)
-
-    result = []
-    for o in orders:
-        entry = {
-            "order_id": o["id"],
-            "name": o["buyerRealName"],
-            "amount": o["amount"],
-        }
-        result.append(entry)
-
-    return result
 
 async def fetch_pending_sell_orders(client: P2P):
     orders_raw = await client.get_pending_orders(
@@ -269,7 +234,7 @@ async def get_chat_message(client: P2P):
         startMessageId=0,
         size=100
     )
-    return msg
+    return msg["result"]["result"]
 
 
 async def send_chat_message(client: P2P):
@@ -320,7 +285,7 @@ async def get_sell_order_id(client: P2P):
         }
 
         result.append(entry)
-        # print("Order id: ", result)
+
     return result
 
 
@@ -335,10 +300,10 @@ async def get_buy_order_id(client: P2P):
         }
 
         result.append(entry)
-        # print("Order id: ", result)
+
     return result
 
-"""FOR SOME REASON RESPONSE WAS [{'orderId': '19920....', 'paymentType': 0}], WHEN 'paymentType has to be '78'"""
+"""paymentType is 0 when the order is open. It's getting the correct paymentType (78) whenever its marked as paid"""
 async def get_pending_sell_order_details(client: P2P):
 
     orders_list = await get_sell_order_id(client=client)
@@ -372,7 +337,7 @@ async def get_pending_sell_order_details(client: P2P):
 
     return result
 
-
+"""paymentType is 0 when the order is open. It's getting the correct paymentType (78) whenever its marked as paid"""
 async def get_pending_buy_order_details(client: P2P):
 
     orders_list = await get_buy_order_id(client=client)
@@ -422,20 +387,6 @@ async def main():
         await fetch_balance(client=api)
           )
 
-    print("List of ads:",
-        await fetch_ads_list(client=api)
-          )
-
-
-    """Not sure if I need two calls from below"""
-    # print("Wise buy ad is here: ",
-    #       await fetch_wise_buy_ad(client=api)
-    #       )
-    #
-    # print("Wise sell ad is here: ",
-    #       await fetch_wise_sell_ad(client=api)
-    #       )
-    ####
 
     print("Wise buy ad details:",
           await fetch_wise_buy_ad_details(client=api)
@@ -445,9 +396,6 @@ async def main():
           await fetch_wise_sell_ad_details(client=api)
           )
 
-    print("Test ad details:",
-          await fetch_test_ad_details(client=api)
-          )
 
     """FOR some reason to remove/activate ad I have to shadow print("mod ads") below"""
 
@@ -478,13 +426,7 @@ async def main():
           await remove_wise_sell_ad(client=api)
           )
 
-    print("Last 10 sell orders:",
-          await fetch_list_of_sell_orders(client=api)
-          )
 
-    print("Counterparty info:",
-          await fetch_bybit_counterparty_info(client=api)
-          )
 
     print("Fetch last 20 Pending sell orders:",
           await fetch_pending_sell_orders(client=api)
@@ -532,85 +474,6 @@ async def main():
         orderId="1992070819939557376"
     ))
 
-    # 11. Release digital asset
-    # print(await api.release_assets(
-    #     orderId="1234567890123456789"
-    # ))
-
-    '''cannot use this one with USD on Wise, not sure about Revolut. Should be good with EUR'''
-    # # 12. Mark order as paid
-    # print(await api.mark_as_paid(
-    #     orderId="1234567890123456789",
-    #     paymentType="123",
-    #     paymentId="5544"
-    # ))
-
-    # # 13. Get chat messages
-    # print(await api.get_chat_messages(
-    #     orderId="1992070819939557376",
-    #     startMessageId=0,
-    #     size=100
-    # ))
-    # print("chat messages")
-
-    # 14. Upload chat file
-    # print(await api.upload_chat_file(
-    #     upload_file="D:/test.png"
-    # ))
-
-    # # 15. Send chat message
-    # import uuid
-    # print(await api.send_chat_message(
-    #     message="Please disregard this message",
-    #     contentType="str",
-    #     orderId="1234567890123456789",
-    #     msgUuid=uuid.uuid4().hex
-    # ))
-
-    # # 16. Post new advertisement
-    # print(await api.post_new_ad(
-    #     tokenId="USDT",
-    #     currencyId="RUB",
-    #     side="1",
-    #     priceType=1,
-    #     premium=90,
-    #     price=78.3,
-    #     minAmount=500,
-    #     maxAmount=3500000,
-    #     remark="Contact @kolya5544 on Telegram once you've paid.",
-    #     tradingPreferenceSet={
-    #             "hasUnPostAd": 0,
-    #             "isKyc": 1,
-    #             "isEmail": 0,
-    #             "isMobile": 0,
-    #             "hasRegisterTime": 0,
-    #             "registerTimeThreshold": 0,
-    #             "orderFinishNumberDay30": 0,
-    #             "completeRateDay30": "",
-    #             "nationalLimit": "",
-    #             "hasOrderFinishNumberDay30": 0,
-    #             "hasCompleteRateDay30": 0,
-    #             "hasNationalLimit": 0
-    #         },
-    #     paymentIds=["6558"],
-    #     quantity="25000",
-    #     paymentPeriod="15",
-    #     itemType="ORIGIN"
-    # ))
-
-    # # 17. Get online advertisements list
-    # print(await api.get_online_ads(
-    #     tokenId="USDT",
-    #     currencyId="RUB",
-    #     side="0",
-    #     payment=["1", "377"],
-    #     vaMaker=True
-    # ))
-
-    # 18. Get user payment
-    print(
-        await api.get_user_payment_types()
-    )
 
     await api.close_session()
 
